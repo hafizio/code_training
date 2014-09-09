e1f00ae7fb7473056bf0fad134818dfa2c280d6e
Hafiz <ibnukamy@gmail.com>
2014-09-09 19:02:06 +0800

    Add new kata

--- fluent_calculator.rb
+++ fluent_calculator.rb
@@ -1,56 +1,29 @@
-module Operation
-include Number
+# The Kata is inspired by Calculating with Functions Kata for JavaScript.
+# The goal is to implement simple calculator which uses fluent syntax:
 
-  def add
-    +
+# Calc.new.one.plus.two # Should return 3
+# Calc.new.five.minus.six # Should return -1
+# Calc.new.seven.times.two # Should return 14
+# Calc.new.nine.divided_by.three # Should return 3
+
+# There are only four operations that are supported (plus, minus,
+# times, divided_by) and 10 digits (zero, one, two, three, four, five,
+# six, seven, eight, nine).
+#
+# Each calculation consists of one operation only.
+require 'minitest/autorun'
+require 'minitest/spec'
+
+describe 'Fluent Calculator' do
+  it 'can add numbers' do
   end
 
-  def minus
-    -
+  it 'can minus numbers' do
   end
 
-  def times
-    *
+  it 'can multiply numbers' do
   end
 
-module Number
-include Operation
-  def one
-    1
+  it 'can divide numbers' do
   end
-  
-  def two
-    2
-  end
-  
-  def three
-    3
-  end
-  
-  def four
-    4
-  end
-  
-  def five
-    5
-  end
-  
-  def six
-    6
-  end
-  
-  def seven
-    7
-  end
-  
-  def eight
-    8
-  end
-  
-  def nine
-  end
-end
-
-class Calc
-include Number
 end
